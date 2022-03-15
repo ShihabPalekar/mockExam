e8e0b41ac5f6bbfa4bcb3b19e1b651a3e8db1f6d
ShihabPalekar <shihabpalekar23@gmail.com>
2022-03-15 15:22:11 +0530

    result and pie chart added

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { PieChart } from "react-minimal-pie-chart";
 import "./result.css";
 
 type Props = {
@@ -6,47 +7,126 @@ type Props = {
 };
 const Result: React.FC<Props> = ({ ...props }) => {
   const [isLoading, setIsLoading] = useState(true);
-  const {data} = props
-  const [total, setTotal] = useState(0);
+  const { data } = props;
+  const [totalMarks, setTotalMarks] = useState(0);
   const [marks, setMarks] = useState(0);
 
   useEffect(() => {
-    for(let i = 0; i < data.length; i++){
-
-      if(data[i].type === "match-up"){
-        let correct = data[i].ansInput?.map((op:any) => op.answer === op.selectedOpt).filter((i:any) => i === true).length
-        setMarks(prevMarks => prevMarks + correct)
-        setTotal(prevTotal => prevTotal + data[i].columnA.length)
-      } 
-      
-      else if (data[i].type === "multi-select") {
-        console.log(JSON.stringify(data[i].answer))
-        console.log(JSON.stringify(data[i].ansInput))
-        if (JSON.stringify(data[i].answer) === JSON.stringify(data[i].ansInput)){
-          console.log("true" ,"multise")
-        } else {console.log("false", "multise")}
-        setTotal(prevTotal => prevTotal + 1)
-      } 
-      
-      else {
-        data[i].answer === data[i].ansInput && 
-        setMarks(prevMarks => prevMarks+ 1)
-        setTotal(prevTotal => prevTotal + 1)
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].type === "match-up") {
+        let correct = data[i].ansInput
+          ?.map((op: any) => op.answer === op.selectedOpt)
+          .filter((i: any) => i === true).length;
+        if (!isNaN(correct)) {
+          setMarks((prevMarks) => prevMarks + 1);
+        }
+        setTotalMarks(
+          (prevTotalMarks) => prevTotalMarks + data[i].columnA.length
+        );
+      } else if (data[i].type === "multi-select") {
+        if (
+          JSON.stringify(data[i].answer.sort()) ===
+          JSON.stringify(data[i].ansInput.sort())
+        ) {
+          setMarks((prevMarks) => prevMarks + 1);
+        }
+        setTotalMarks((prevTotalMarks) => prevTotalMarks + 1);
+      } else {
+        data[i].answer === data[i].ansInput &&
+          setMarks((prevMarks) => prevMarks + 1);
+        setTotalMarks((prevTotalMarks) => prevTotalMarks + 1);
       }
     }
-    setIsLoading(false)
+    setIsLoading(false);
   }, []);
 
-  console.log(total)
-  
+  console.log(marks);
+
   if (isLoading) {
     return <p>Calculating result...</p>;
   }
 
   return (
     <div className="results-wrapper">
-      <div className="correct-ans">ans</div>
-      <div className="pie">pie</div>
+      <div className="correct-ans">
+        <h2>Answers</h2>
+        {data.map((i: any, index: number) => {
+          if (i.type === "match-up") {
+            return (
+              <div>
+                <p>{index + 1}.</p>
+                <div>
+                  <p>Your answer:</p>
+                  {i.ansInput.map((a: any) => (
+                    <span style={{ marginRight: "10px" }}>{a.selectedOpt}</span>
+                  ))}
+                </div>
+                <div>
+                  <p>Correct answer:</p>
+                  {i.ansInput.map((a: any) => (
+                    <span style={{ marginRight: "10px" }}>{a.answer}</span>
+                  ))}
+                </div>
+              </div>
+            );
+          } else if (i.type === "multi-select") {
+            return (
+              <div>
+                <p>{index + 1}.</p>
+                <div>
+                  <span style={{ marginRight: "10px" }}>Your answer:</span>
+                  {i.ansInput.map((a: string) => (
+                    <span style={{ marginRight: "10px" }}>{a}</span>
+                  ))}
+                </div>
+                <br />
+                <div>
+                  <span style={{ marginRight: "10px" }}>Correct answer:</span>
+                  {i.answer.map((a: string) => (
+                    <span style={{ marginRight: "10px" }}>{a}</span>
+                  ))}
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div>
+                <p>{index + 1}.</p>
+                <div>
+                  <span style={{ marginRight: "10px" }}>Your answer:</span>
+                  <span>{i.ansInput}</span>
+                </div>
+                <br />
+                <div>
+                  <span style={{ marginRight: "10px" }}>Correct answer:</span>
+                  <span>{i.answer}</span>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="pie">
+        <h2>Result</h2>
+        <h3 style={{marginTop: "25px"}}>Your total score is {marks} out of {totalMarks}.</h3>
+        <PieChart
+          totalValue={totalMarks}
+          data={[
+            {
+              title: `Correct answers ${(marks / totalMarks) * 100}`,
+              value: marks,
+              color: "green",
+            },
+            {
+              title: `Incorrect answers  ${
+                ((totalMarks - marks) / totalMarks) * 100
+              }`,
+              value: totalMarks - marks,
+              color: "red",
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 };
--- src/questionsData.tsx
+++ src/questionsData.tsx
@@ -39,8 +39,86 @@ const questionsDataEnglish: any = [
   },
 ];
 
-const questionsDataHindi: any = [];
+const questionsDataHindi: any = [
+  {
+    id: 1,
+    type: "mcq",
+    que: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    options: ["dummy1", "dummy2", "dummy3", "dummy4"],
+    answer: "dummy3",
+  },
+  {
+    id: 2,
+    type: "fill-up",
+    que: "Donec pede justo, fringilla _________ aliquet nec, vulputate eget, arcu.",
+    answer: "ans",
+  },
+  {
+    id: 3,
+    type: "boolean",
+    que: "Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.",
+    answer: "true",
+  },
+  {
+    id: 4,
+    type: "match-up",
+    que: "Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.",
+    columnA: [
+      { id: "Q1", opt: "Apple", answer: "Fruit" },
+      { id: "Q2", opt: "Cat", answer: "Animal" },
+      { id: "Q3", opt: "Red", answer: "Color" },
+      { id: "Q4", opt: "India", answer: "Country" },
+    ],
+    columnB: ["Color", "Animal", "Country", "Fruit"],
+  },
+  {
+    id: 5,
+    type: "multi-select",
+    que: "Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.",
+    options: ["dummy1", "dummy2", "dummy3", "dummy4"],
+    answer: ["dummy2", "dummy4"],
+  },
+];
 
-const questionsDataSpanish: any = [];
+const questionsDataSpanish: any = [
+  {
+    id: 1,
+    type: "mcq",
+    que: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    options: ["dummy1", "dummy2", "dummy3", "dummy4"],
+    answer: "dummy3",
+  },
+  {
+    id: 2,
+    type: "fill-up",
+    que: "Donec pede justo, fringilla _________ aliquet nec, vulputate eget, arcu.",
+    answer: "ans",
+  },
+  {
+    id: 3,
+    type: "boolean",
+    que: "Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.",
+    answer: "true",
+  },
+  {
+    id: 4,
+    type: "match-up",
+    que: "Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.",
+    columnA: [
+      { id: "Q1", opt: "Apple", answer: "Fruit" },
+      { id: "Q2", opt: "Cat", answer: "Animal" },
+      { id: "Q3", opt: "Red", answer: "Color" },
+      { id: "Q4", opt: "India", answer: "Country" },
+    ],
+    columnB: ["Color", "Animal", "Country", "Fruit"],
+  },
+  {
+    id: 5,
+    type: "multi-select",
+    que: "Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.",
+    options: ["dummy1", "dummy2", "dummy3", "dummy4"],
+    answer: ["dummy2", "dummy4"],
+  },
+];
 
 export { questionsDataEnglish, questionsDataHindi, questionsDataSpanish };
